2c1984b9d8fdc797bbe27c7468507c0c030f1719
Daniela Rodríguez <danaronait2002@gmail.com>
2022-10-21 13:49:32 +0100

    6º Commit (Avances en clase)
    - Cambio color fondo de título y botones
    - Interfaz inprovement

---
+++ Interfaz/Assets/Scripts/ButtonsSize.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsSize : MonoBehaviour
+{
+    public void ScaleEnter()
+    {
+        LeanTween.scale(gameObject, Vector3.one * 1.2f, 1f).setEaseInBounce();
+    }
+
+    public void ScaleExit()
+    {
+        LeanTween.cancel(gameObject);
+        LeanTween.scale(gameObject, Vector3.one, 0.25f).setEaseOutBack();
+    }
+}
